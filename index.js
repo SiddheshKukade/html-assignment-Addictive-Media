a9eac33d75ca7eb9faa0958d9cecb014d69ff5d5
SiddheshKukade <littlelord124@gmail.com>
2022-04-28 22:45:48 +0530

    Feat: Cards

---
+++ index.js
@@ -0,0 +1,143 @@
+console.log("go");
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['BACK', 'NEXT'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+  constructor(container, items, controls) {
+    this.carouselContainer = container;
+    this.carouselControls = controls;
+    this.carouselArray = [...items];
+  }
+
+  // Update css classes for gallery
+  updateGallery() {
+    this.carouselArray.forEach(el => {
+      el.classList.remove('gallery-item-1');
+      el.classList.remove('gallery-item-2');
+      el.classList.remove('gallery-item-3');
+      el.classList.remove('gallery-item-4');
+      el.classList.remove('gallery-item-5');
+    });
+
+    this.carouselArray.slice(0, 5).forEach((el, i) => {
+      el.classList.add(`gallery-item-${i+1}`);
+    });
+  }
+
+  // Update the current order of the carouselArray and gallery
+  setCurrentState(direction) {
+
+    if (direction.className == 'gallery-controls-previous') {
+      this.carouselArray.unshift(this.carouselArray.pop());
+    } else {
+      this.carouselArray.push(this.carouselArray.shift());
+    }
+    
+    this.updateGallery();
+  }
+
+  // Construct the carousel navigation
+  // setNav() {
+    // galleryContainer.appendChild(document.createElement('ul')).className = 'gallery-nav';
+
+    // this.carouselArray.forEach(item => {
+    //   const nav = galleryContainer.lastElementChild;
+    //   nav.appendChild(document.createElement('li'));
+    // }); 
+  // }s
+
+  // Construct the carousel controls
+  setControls() {
+    this.carouselControls.forEach(control => {
+      galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+
+      document.querySelector(`.gallery-controls-${control}`).innerHTML = control;
+    });
+  }
+ 
+  // Add a click event listener to trigger setCurrentState method to rearrange carousel
+  useControls() {
+    const triggers = [...galleryControlsContainer.childNodes];
+
+    triggers.forEach(control => {
+      control.addEventListener('click', e => {
+        e.preventDefault();
+
+        if (control.className == 'gallery-controls-add') {
+          const newItem = document.createElement('img');
+          const latestItem = this.carouselArray.length;
+          const latestIndex = this.carouselArray.findIndex(item => item.getAttribute('data-index') == this.carouselArray.length)+1;
+
+          // Assign the necessary properties for new gallery item
+          Object.assign(newItem,{
+            className: 'gallery-item',
+            src: `http://fakeimg.pl/300/?text=${this.carouselArray.length+1}`
+          });
+          newItem.setAttribute('data-index', this.carouselArray.length+1);
+
+          // Then add it to the carouselArray and update the gallery
+          this.carouselArray.splice(latestIndex, 0, newItem);
+          document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
+          this.updateGallery();
+
+        } else {
+          this.setCurrentState(control);
+        }
+
+      });
+    });
+  }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+// exampleCarousel.setNav();
+exampleCarousel.useControls();
+
+
+// let slidePosition = 0;
+// const slides = document.getElementsByClassName('carousel__item');
+// const totalSlides = slides.length;
+
+// document.
+//   getElementById('carousel__button--next')
+//   .addEventListener("click", function () {
+//     moveToNextSlide();
+//   });
+// document.
+//   getElementById('carousel__button--prev')
+//   .addEventListener("click", function () {
+//     moveToPrevSlide();
+//   });
+
+// function updateSlidePosition() {
+//   for (let slide of slides) {
+//     slide.classList.remove('carousel__item--visible');
+//     slide.classList.add('carousel__item--hidden');
+//   }
+
+//   slides[slidePosition].classList.add('carousel__item--visible');
+// }
+
+// function moveToNextSlide() {
+//   if (slidePosition === totalSlides - 1) {
+//     slidePosition = 0;
+//   } else {
+//     slidePosition++;
+//   }
+
+//   updateSlidePosition();
+// }
+
+// function moveToPrevSlide() {
+//   if (slidePosition === 0) {
+//     slidePosition = totalSlides - 1;
+//   } else {
+//     slidePosition--;
+//   }
+
+//   updateSlidePosition();
+// }
